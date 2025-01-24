d76ee31edda519da6e014365825f99abbbddf415
MZTYA2001 <mohamedziadtariq@gmail.com>
2025-01-24 07:27:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ if human_input := st.chat_input("Ask something about the document"):
                 for i, doc in enumerate(response["context"]):
                     page_number = doc.metadata.get("page", "unknown")  # Get page number from metadata
                     st.write(f"According to Page: {page_number}")
-                    st.write(doc.page_content)
+                    # st.write(doc.page_content)
                     st.write("--------------------------------")
             else:
                 st.write("No context available.")
